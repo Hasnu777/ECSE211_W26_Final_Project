7ebb95faa92d5e3376abfc9907ba547af30afefd
Hasnu777 <hassanashah10@gmail.com>
2026-02-26 13:16:12 -0500

    Basic gyro sensor testing file created, gyro sensor module added to imports in main.py

--- Gyro_Sensor_Testing.py
+++ Gyro_Sensor_Testing.py
@@ -2,3 +2,9 @@ from utils import sound
 from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, EV3GyroSensor, wait_ready_sensors
 import time
 import threading
+
+TS1 = TouchSensor(2)
+Gyro = EV3GyroSensor(1,mode="both")
+
+while True:
+    print(Gyro.get_raw_value())
