77b775e090ec8d5e9f3bb5440ab9ffc7cc6fc00c
Hasnu777 <hassanashah10@gmail.com>
2026-02-26 14:00:40 -0500

    Developed version 1 of proper testing file

--- Gyro_Sensor_Testing.py
+++ Gyro_Sensor_Testing.py
@@ -6,8 +6,35 @@ import threading
 TS1 = TouchSensor(2)
 Gyro = EV3GyroSensor(1,mode="both")
 
-while True:
-    if TS1.is_pressed():
-        break
-    print(Gyro.get_raw_value())
-    time.sleep(0.5)
+GYRO_SENSOR_DATA_FILE = "../gyro_sensor_data_file.csv"
+
+print("Testing file running. Waiting for sensor initialisation...")
+
+wait_ready_sensors(True)
+print("Sensors. Initialized.")
+
+def collect_continuous_gyro_data():
+    try:
+        output_file = open(GYRO_SENSOR_DATA_FILE,"w")
+        output_file.write(f"Degrees Rotated Since Beginning,Degrees Rotated Per Second\n")
+        while not TS1.is_pressed():
+            pass
+        time.sleep(0.5)
+        print("Collecting readings now.")
+        while not TS1.is_pressed():
+            gyro_data = Gyro.get_both_measure()
+            if gyro_data is not None:
+                print(gyro_data)
+                output_file.write(f"{gyro_data[0]},{gyro_data[1]}\n")
+            time.sleep(0.5)
+    except BaseException as e:
+        pass
+    finally:
+        print("Gyro samples collected.")
+        output_file.close()
+        print("Testing complete.")
+        reset_brick()
+        exit()
+
+if __name__ == "__main__":
+    collect_continuous_gyro_data()
