03c288d2dd5a5e4857e9086f0d1fb02e99d157a2
matt-yst <matthew.ys.tan@gmail.com>
2026-03-16 16:17:47 -0400

    initial commit

--- main.py
+++ main.py
@@ -2,4 +2,32 @@ from utils import sound
 from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sensors
 import time
 import threading
-#hello branch
+
+# -------------------- SENSORS AND MOTORS --------------------
+m1 = Motor("A")
+m2 = Motor("B")
+
+
+m1.setlimits()
+m2.setlimits()
+
+# -------------------- FUNCTIONS --------------------
+
+
+def main():
+    m1.set_dps(720)
+    m2.set_dps(720)
+    time.sleep(3)
+    m1.set_dps(0)
+    m2.set_dps(0)
+
+    # rotate
+    m1.set_position(180)
+    m2.set_position(-180)
+
+
+    
+
+
+
+
