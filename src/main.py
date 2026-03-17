eb6304de77dc924d94535afae7b44483eb15b2e4
Hassan Shah <hassanashah10@gmail.com>
2026-03-17 17:40:27 -0400

    Moved drive_system_testing code to drive_system_testing.py

--- src/main.py
+++ src/main.py
@@ -2,38 +2,3 @@ from utils import sound
 from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sensors
 import time
 import threading
-
-# -------------------- SENSORS AND MOTORS --------------------
-m1 = Motor("A")
-m2 = Motor("B")
-
-# m1.setlimits()
-# m2.setlimits()
-
-# -------------------- FUNCTIONS --------------------
-
-
-def main():
-    m1.reset_encoder()
-    m2.reset_encoder()
-    m1.set_position(-360)
-    m2.set_position(360)
-    #m1.set_dps(720)
-    #m2.set_dps(720)
-    #time.sleep(3)
-    #m1.set_dps(0)
-    #m2.set_dps(0)
-
-    # rotate
-
-
-if __name__ == "__main__":
-    print("test")
-    main()
-
-
-    
-
-
-
-
