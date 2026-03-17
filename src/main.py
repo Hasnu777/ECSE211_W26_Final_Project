3478c70cf836b9c73b34b1655c5a58d27e7f2c82
15centmcgill <15centmcgill@gmail.com>
2026-03-17 17:47:36 +0000

    claw code was here for some reason

--- src/main.py
+++ src/main.py
@@ -7,24 +7,22 @@ import threading
 m1 = Motor("A")
 m2 = Motor("B")
 
-
-m1.setlimits()
-m2.setlimits()
+# m1.reset_encoder();
+# m2.reset_encoder();
+# m1.setlimits()
+# m2.setlimits()
 
 # -------------------- FUNCTIONS --------------------
 
 
-def main():
-    m1.set_dps(720)
-    m2.set_dps(720)
-    time.sleep(3)
-    m1.set_dps(0)
-    m2.set_dps(0)
-
-    # rotate
-    m1.set_position(180)
-    m2.set_position(-180)
-
+# rotate
+while (True):
+    m1.set_position(-80)
+    m2.set_position(-50)
+    time.sleep(1)
+    m1.set_position(0)
+    m2.set_position(0)
+    time.sleep(1)
 
     
 
