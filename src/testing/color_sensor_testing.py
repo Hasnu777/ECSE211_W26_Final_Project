f6fadb7ca9247fc9f4a1d4059a31c83ebf342fb9
15centmcgill <15centmcgill@gmail.com>
2026-03-18 20:12:21 +0000

    read some colours

--- src/testing/color_sensor_testing.py
+++ src/testing/color_sensor_testing.py
@@ -1,5 +1,5 @@
-from src.utils import sound
-from src.utils.brick import EV3ColorSensor, TouchSensor, wait_ready_sensors, reset_brick
+from utils import sound
+from utils.brick import EV3ColorSensor, TouchSensor, wait_ready_sensors, reset_brick
 from time import sleep
 import threading
 import csv
@@ -7,34 +7,32 @@ import csv
 # CONSTANTS
 ColorSensor = EV3ColorSensor(1)
 TouchSensor = TouchSensor(2)
-DataFile = "../data/calibration_data_v1.csv" # Change data file if needed
+DataFile = "calibration_data_yellow.csv"
 
 wait_ready_sensors(True)
 
 def collect_color_data():
     try:
         with open(DataFile, "a", newline='') as csvfile:
-            # csv module
             writer = csv.writer(csvfile)
-            # headers
-            writer.writerow(["R", "G", "B"])
+            #writer.writerow(["R", "G", "B"])
             print("Press touch sensor to read color data")
             while not TouchSensor.is_pressed():
                 pass
-            print("Pressed. Reading data now in 3 second intervals.")
-            while True:
-                # Press to stop, meanwhile read in 3 second intervals
+            print("Pressed. Reading data now in 2 second intervals.")
+            count = 0
+            while count < 30:
                 if not TouchSensor.is_pressed():
                     data = ColorSensor.get_rgb()
                     if data is not None:
                         writer.writerow(data)
+                        count += 1
                     else:
                         print("Color failed to read, try again.")
-                    sleep(3)
+                    sleep(2)
     except BaseException as e:
         print("Exception occurred:", e)
     finally:
-        # Close & save file, reset brick
         csvfile.close()
         reset_brick()
         exit()
