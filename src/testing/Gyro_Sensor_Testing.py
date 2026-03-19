51b7a7615e0a3627c97e12ef3b718d4e26b892af
Hassan Shah <hassanashah10@gmail.com>
2026-03-19 17:12:38 -0400

    Changing rotation value 292 -> 583

--- src/testing/Gyro_Sensor_Testing.py
+++ src/testing/Gyro_Sensor_Testing.py
@@ -19,62 +19,62 @@ print("Sensors initialized.")
 def perform_rotations():
 
     # 90 degree rotation about face
-    motor1.set_position_relative(292)
-    motor2.set_position_relative(-292)
+    motor1.set_position_relative(583)
+    motor2.set_position_relative(-583)
 
     time.sleep(3)
 
     # reverse 90 degree rotation about face
-    motor1.set_position_relative(-292)
-    motor2.set_position_relative(292)
+    motor1.set_position_relative(-583)
+    motor2.set_position_relative(583)
 
     time.sleep(3)
 
     # 90 degree pivot
-    motor1.set_position_relative(292)
+    motor1.set_position_relative(583)
     motor2.set_position_relative(0)
 
     time.sleep(3)
 
     # reverse 90 degree pivot
-    motor1.set_position_relative(-292)
+    motor1.set_position_relative(-583)
     motor2.set_position_relative(0)
 
     time.sleep(3)
 
     # 90 degree pivot other way
     motor1.set_position_relative(0)
-    motor2.set_position_relative(292)
+    motor2.set_position_relative(583)
 
     time.sleep(3)
 
     # reverse 90 degree pivot other way
     motor1.set_position_relative(0)
-    motor2.set_position_relative(-292)
+    motor2.set_position_relative(-583)
 
     time.sleep(3)
 
     # 180 degree rotation about face
-    motor1.set_position_relative(292*2)
-    motor2.set_position_relative(-292*2)
+    motor1.set_position_relative(583*2)
+    motor2.set_position_relative(-583*2)
 
     time.sleep(3)
 
     # reverse 180 degree rotation about face
-    motor1.set_position_relative(-292*2)
-    motor2.set_position_relative(292*2)
+    motor1.set_position_relative(-583*2)
+    motor2.set_position_relative(583*2)
 
     time.sleep(3)
 
     # 360 degree rotation about face
-    motor1.set_position_relative(292*4)
-    motor2.set_position_relative(-292*4)
+    motor1.set_position_relative(583*4)
+    motor2.set_position_relative(-583*4)
 
     time.sleep(3)
 
     # reverse 360 degree rotation about face
-    motor1.set_position_relative(-292*4)
-    motor2.set_position_relative(292*4)
+    motor1.set_position_relative(-583*4)
+    motor2.set_position_relative(583*4)
 
     motor1.set_dps(120)
     motor2.set_dps(120)
