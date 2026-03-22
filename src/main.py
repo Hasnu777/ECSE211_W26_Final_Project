58eb96722e4655464bd9f47ee234bf069f6328f5
Sonia Ly <sonia.ly@mail.mcgill.ca>
2026-03-21 22:47:46 -0400

    commented code

--- src/main.py
+++ src/main.py
@@ -3,78 +3,63 @@ from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sens
 import time
 import threading
 
-m1 = Motor("C")
+# ---------- MOTOR SETUP ----------
+arm = Motor("C")
 gripper = Motor("D")
-m1.set_limits(power=30)
+arm.set_limits(power=30)
 gripper.set_limits(power=30)
-#m1.reset_encoder()
 
-def setup():
-    m1.set_position(0)
+# ---------- CONSTANTS (angles at which we open/close) ----------
+OPENED_GRIPPER = -50
+CLOSED_GRIPPER = 0
+RAISED_ARM = 180
+LOWERED_ARM = 0
+
+# ---------- FUNCTION DEFINITIONS ----------
+def open_gripper():
+    gripper.set_position(OPENED_GRIPPER)
+
+def close_gripper():
+    gripper.set_position(CLOSED_GRIPPER)
     
-def loop_grab():
-    while(True):
-        print("test")
-        m1.set_position(70) #if want claw closer, increase absolute value
-        time.sleep(0.5)
-        m1.set_position(0)
-        time.sleep(0.5)
+def raise_arm():
+    arm.set_position(RAISED_ARM)
 
-def single_grab():
-    m1.set_position(-110) #if want claw closer, increase absolute value
+def lower_arm():
+    arm.set_position(LOWERED_ARM)
 
-def reset_position_of_claw():
-    m1.set_position(0)
-    
 
+# ---------- FUNCTION DEFINITIONS ----------
 if __name__ == "__main__":
-    setup() # Should never be commented
-    #loop_grab() # Uncomment to grap in a loop
-    #single_grab() # Uncomment to grap once
-    #reset_position_of_claw()
-    
-    # Wrist Movement
-#     time.sleep(1)
-#     while(True):
-#         gripper.set_position(-50)
-#         time.sleep(1)
-#         gripper.set_position(10)
-#         time.sleep(1)
-        
+    open_gripper()
     time.sleep(1)
-    
-#    gripper.set_position(-50)
-#     m1.set_position(0)
-#     time.sleep(3)
-#     gripper.set_position(-50)
-#     time.sleep(2)
-    gripper.set_position(10)
+    close_gripper()
     time.sleep(1)
-    m1.set_position(180)
+    raise_arm()
     time.sleep(1)
-    gripper.set_position(-50)
-    time.sleep(1)
-    m1.set_position(0)
-    time.sleep(1)
-    gripper.set_position(10)
-    time.sleep(2)
-    gripper.set_position(-50)
-    m1.set_position(185)
-    time.sleep(1)
-    gripper.set_position(10)
-    time.sleep(1)
-    m1.set_position(0)
-    time.sleep(1)
-    gripper.set_position(-50)
+    open_gripper()
+    lower_arm(1)
+
+    # gripper.set_position(10)
+    # time.sleep(1)
+    # wrist.set_position(180)
+    # time.sleep(1)
+    # gripper.set_position(-50)
+    # time.sleep(1)
+    # wrist.set_position(0)
+    # time.sleep(1)
+    # gripper.set_position(10)
+    # time.sleep(2)
+    # gripper.set_position(-50)
+    # wrist.set_position(185)
+    # time.sleep(1)
+    # gripper.set_position(10)
+    # time.sleep(1)
+    # wrist.set_position(0)
+    # time.sleep(1)
+    # gripper.set_position(-50)
 
     
-    # Shoulder Movement
-#     time.sleep(1)
-#     while(True):
-#         m1.set_position(180)
-#         time.sleep(1)
-#         m1.set_position(0)
-#         time.sleep(1)
         
 
 
