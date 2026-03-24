724f3e78a4b16bd22b16aa0551daaad7ac915e43
15centmcgill <15centmcgill@gmail.com>
2026-03-24 22:29:40 +0000

    Current state of hardcoding is done, gyro and wiggling required now

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ import threading
 # ------------------------- CONSTANTS ------------------------
 
 ONE_SQUARE = 710
-NINETY_DEGREES = 338
+NINETY_DEGREES_LEFT = 338
+NINETY_DEGREES_RIGHT = 330
 
 # -------------------- SENSORS AND MOTORS --------------------
 rightWheel = Motor("A")
@@ -31,14 +32,22 @@ def moveBackward(amount):
     print(f"Moved backward {amount / ONE_SQUARE} blocks.")
 
 def turnRight(amount):
+    leftWheel.set_limits(power=30,dps=300)
+    rightWheel.set_limits(power=30,dps=300)
     leftWheel.set_position_relative(amount)
     rightWheel.set_position_relative(-amount)
-    print(f"Turned right by { 90 * (amount / NINETY_DEGREES)} degrees.")
+    leftWheel.set_limits(power=50,dps=425)
+    rightWheel.set_limits(power=50,dps=425)
+    print(f"Turned right by { 90 * (amount / NINETY_DEGREES_LEFT)} degrees.")
 
 def turnLeft(amount):
+    leftWheel.set_limits(power=30,dps=300)
+    rightWheel.set_limits(power=30,dps=300)
     rightWheel.set_position_relative(amount)
     leftWheel.set_position_relative(-amount)
-    print(f"Turned left by {90 * (amount / NINETY_DEGREES)} degrees.")
+    rightWheel.set_limits(power=50,dps=425)
+    leftWheel.set_limits(power=50,dps=425)
+    print(f"Turned left by {90 * (amount / NINETY_DEGREES_LEFT)} degrees.")
 
 def main():
     rightWheel.reset_encoder()
@@ -48,17 +57,17 @@ def main():
     time.sleep(3)
     # TODO: PICK UP BLOCKS AND RESET
     # Turning to face outside of pharmacy and move out of it (2 -> 3)
-    turnLeft(NINETY_DEGREES)
+    turnLeft(NINETY_DEGREES_LEFT)
     time.sleep(3)
     moveForward(ONE_SQUARE * 1.75) # exit pharmacy and move forward
     time.sleep(4)
     # Turning right and moving forward (3 -> 4)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     time.sleep(3)
     moveForward(ONE_SQUARE * 1.9)
     time.sleep(4)
     # Turning right and moving into double room (4 -> 5)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT-3)
     time.sleep(3)
     moveForward(ONE_SQUARE * 0.5)
     time.sleep(3)
@@ -66,66 +75,66 @@ def main():
     # NOT DOING IT RIGHT NOW, JUST MOVING IN AND OUT
     # scanning room in sections
     # section 1
-    moveForward(ONE_SQUARE * 1.7)
+    moveForward(ONE_SQUARE * 1.2)
     time.sleep(4)
     print("Scanned for beds. No beds found. Scanning new section...")
     # Move to section 2
-    moveBackward(ONE_SQUARE * 1.7)
+    moveBackward(ONE_SQUARE * 1.2)
     time.sleep(4)
-    turnLeft(NINETY_DEGREES)
+    turnLeft(NINETY_DEGREES_LEFT)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 0.5)
+    moveForward(ONE_SQUARE * 0.4)
     time.sleep(3)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 1.7)
+    moveForward(ONE_SQUARE * 1.2)
     time.sleep(4)
     print("Scanned for beds. No beds found. Scanning new section...")
     # Move to section 3
-    moveBackward(ONE_SQUARE * 1.7)
+    moveBackward(ONE_SQUARE * 1.2)
     time.sleep(4)
-    turnLeft(NINETY_DEGREES)
+    turnLeft(NINETY_DEGREES_LEFT)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 0.5)
+    moveForward(ONE_SQUARE * 0.4)
     time.sleep(3)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 1.7)
+    moveForward(ONE_SQUARE * 1.2)
     time.sleep(4)
     print("Scanned for beds. No beds found. Exiting room...")
-    moveBackward(ONE_SQUARE * 1.7)
+    moveBackward(ONE_SQUARE * 1.2)
     time.sleep(4)
-    # moveBackward(ONE_SQUARE * 0.5)
+    moveBackward(ONE_SQUARE * 0.5)
     time.sleep(3)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     # Moving to single room
     time.sleep(3)
     moveForward(ONE_SQUARE * 1.7)
     time.sleep(4)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     time.sleep(3)
     moveForward(ONE_SQUARE * 0.5)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 1.7)
+    moveForward(ONE_SQUARE * 1.2)
     time.sleep(4)
     print("Scanned for beds. No beds found. Exiting room...")
-    moveBackward(ONE_SQUARE * 1.7)
+    moveBackward(ONE_SQUARE * 1.2)
     time.sleep(4)
     moveBackward(ONE_SQUARE * 0.5)
     # Moving to other single room
     time.sleep(3)
-    turnLeft(NINETY_DEGREES)
+    turnLeft(NINETY_DEGREES_LEFT)
     time.sleep(3)
     moveForward(ONE_SQUARE * 2)
     time.sleep(4)
-    turnRight(NINETY_DEGREES)
+    turnRight(NINETY_DEGREES_RIGHT)
     time.sleep(3)
     moveForward(ONE_SQUARE * 0.5)
     time.sleep(3)
-    moveForward(ONE_SQUARE * 1.7)
+    moveForward(ONE_SQUARE * 1.2)
     time.sleep(4)
     print("Scanned for beds. No beds found. Exiting room...")
-    moveBackward(ONE_SQUARE * 1.7)
+    moveBackward(ONE_SQUARE * 1.2)
     time.sleep(4)
     # Returning to pharmacy
     moveBackward(ONE_SQUARE * 0.5)
