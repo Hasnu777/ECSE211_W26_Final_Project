38ce584a545265dc7d259a6848fa83821f2e5619
Afrasiyab05 <afrasiyab05@hotmail.com>
2026-03-24 12:41:39 -0400

    Moved code into the correct file (main.py) and commented old code

--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@ from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sens
 import time
 import threading
 
+
 # -------------------- SENSORS AND MOTORS --------------------
 m1 = Motor("A")
 m2 = Motor("B")
 
-m1.set_limits(power=50)
-m2.set_limits(power=50)
+# m1.setlimits()
+# m2.setlimits()
 
 # -------------------- FUNCTIONS --------------------
 
@@ -16,9 +17,26 @@ m2.set_limits(power=50)
 def main():
     m1.reset_encoder()
     m2.reset_encoder()
-    m1.set_position(-335)
-    m2.set_position(335)
-    #m1.set_dps(720)
+# Moving from 1 to 2
+    m1.set_position(360)
+    m2.set_position(360)
+    time.sleep(2)
+# Turning from 2 to 3 and moving
+    m1.set_position(90)
+    time.sleep(1)
+    m1.set_position(360)
+    m2.set_position(360)
+#Turning 3 to 4 and moving
+    m2.set_position(720)
+    m1.set_position(720)
+    time.sleep(1)
+    m2.set_position(90)
+    time.sleep(1)
+    m1.set_position(720)
+    m2.set_position(720)
+
+#Moving 4    
+#m1.set_dps(720)
     #m2.set_dps(720)
     #time.sleep(3)
     #m1.set_dps(0)
@@ -30,3 +48,36 @@ def main():
 if __name__ == "__main__":
     print("test")
     main()
+
+#from utils import sound
+#from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sensors
+#import time
+#import threading
+
+# -------------------- SENSORS AND MOTORS --------------------
+#m1 = Motor("A")
+#m2 = Motor("B")
+
+#m1.set_limits(power=50)
+#m2.set_limits(power=50)
+
+# -------------------- FUNCTIONS --------------------
+
+
+#def main():
+#    m1.reset_encoder()
+#    m2.reset_encoder()
+#    m1.set_position(-335)
+#    m2.set_position(335)
+    #m1.set_dps(720)
+    #m2.set_dps(720)
+    #time.sleep(3)
+    #m1.set_dps(0)
+    #m2.set_dps(0)
+
+    # rotate
+
+
+#if __name__ == "__main__":
+#    print("test")
+#    main()
