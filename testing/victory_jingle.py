2ea9cbeeb73c1da387ca0ebba267a68f399798b7
DPM group 24 <dpm-24@example.com>
2026-03-29 03:09:02 +0000

    cleanup washing machine jingle -Matt

--- testing/victory_jingle.py
+++ testing/victory_jingle.py
@@ -9,25 +9,25 @@ import threading
 import time
 
 # -------------------- CONSTANTS --------------------
-VOLUME = 90
+VOLUME = 60
 
-E6 = Sound(duration=0.3, pitch="E6", volume=VOLUME)
-E6_HALF = Sound(duration=0.3, pitch="E6", volume=VOLUME)
-E6_3HALVES = Sound(duration=0.45, pitch="E6", volume=VOLUME)
-E6_3X = Sound(duration=0.9, pitch="E6", volume=VOLUME)
-A7 = Sound(duration=0.3, pitch="A7", volume=VOLUME)
-A7_HALF = Sound(duration=0.15, pitch="A7", volume=VOLUME)
-A7_3X = Sound(duration=0.9, pitch="A7", volume=VOLUME)
-A7_2X = Sound(duration=0.6, pitch="A7", volume=VOLUME)
-C7 = Sound(duration=0.3, pitch="C#7", volume=VOLUME)
-B7 = Sound(duration=0.3, pitch="B7", volume=VOLUME)
-B7_HALF = Sound(duration=0.15, pitch="B7", volume=VOLUME)
-G6 = Sound(duration=0.3, pitch="G#6", volume=VOLUME)
-G6_HALF = Sound(duration=0.15, pitch="G#6", volume=VOLUME)
-F6_HALF = Sound(duration=0.15, pitch="F#6", volume=VOLUME)
-F6 = Sound(duration=0.3, pitch="F#6", volume=VOLUME)
-D6 = Sound(duration=0.3, pitch="D#6", volume=VOLUME)
-D7_HALF = Sound(duration=0.15, pitch="D#7", volume=VOLUME)
+E6 = Sound(duration=0.3, pitch="E6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+E6_HALF = Sound(duration=0.3, pitch="E6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+E6_3HALVES = Sound(duration=0.45, pitch="E6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+E6_3X = Sound(duration=0.9, pitch="E6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+A7 = Sound(duration=0.3, pitch="A7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+A7_HALF = Sound(duration=0.15, pitch="A7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+A7_3X = Sound(duration=0.9, pitch="A7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+A7_2X = Sound(duration=0.6, pitch="A7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+C7 = Sound(duration=0.3, pitch="C#7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+B7 = Sound(duration=0.3, pitch="B7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+B7_HALF = Sound(duration=0.15, pitch="B7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+G6 = Sound(duration=0.3, pitch="G#6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+G6_HALF = Sound(duration=0.15, pitch="G#6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+F6_HALF = Sound(duration=0.15, pitch="F#6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+F6 = Sound(duration=0.3, pitch="F#6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+D6 = Sound(duration=0.3, pitch="D#6", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
+D7_HALF = Sound(duration=0.15, pitch="D#7", volume=VOLUME, amp_f = 5, amp_ka = 0.1, amp_ac = 0.9)
 
 def victory_jingle():
     measure1 = [E6, A7, A7, C7, C7, A7_2X, E6, E6, E6_3HALVES, E6_HALF, B7_HALF, A7_HALF, G6_HALF, F6_HALF, E6_3X]
@@ -42,13 +42,13 @@ def victory_jingle():
     song5 = Song(measure5)
     song1.compile()
     song1.play()
-    time.sleep(5.1)
+    time.sleep(5.0)
     song2.compile()
     song2.play()
-    time.sleep(4.4)
+    time.sleep(4.5)
     song3.compile()
     song3.play()
-    time.sleep(4.8)
+    time.sleep(4.3)
     song4.compile()
     song4.play()
     time.sleep(4.5)
@@ -57,11 +57,12 @@ def victory_jingle():
     time.sleep(4.8)
     
 
-# def task_jingle():
-#     sounds = [sound.Sound()]
-#     song = sound.Song(sounds)
-#     song.compile
-#     song.play() 
+def task_jingle():
+    sounds = [sound.Sound()]
+    song = sound.Song(sounds)
+    song.compile
+    song.play()
+    
 if __name__ == "__main__":
 #     tone1 = Sound(duration=0.3, pitch="E6", volume=VOLUME)
 #     tone1.play()
