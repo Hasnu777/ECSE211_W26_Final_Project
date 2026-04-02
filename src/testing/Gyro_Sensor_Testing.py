1d402bea24b6950e595cefefbfaed64c059bdf51
DPM group 24 <dpm-24@example.com>
2026-04-02 13:33:02 +0000

    experimenting with dps measurement

--- src/testing/Gyro_Sensor_Testing.py
+++ src/testing/Gyro_Sensor_Testing.py
@@ -22,8 +22,8 @@ print("Sensors initialized.")
 
 
 def perform_rotations():
-    motor_R.set_limits(power=50)
-    motor_L.set_limits(power=50)
+    motor_R.set_limits(power=100)
+    motor_L.set_limits(power=100)
 
     Gyro.set_mode("abs")
 
@@ -36,9 +36,10 @@ def perform_rotations():
 
 
 def continuous_measure_gyro():
-    global target, running
+    global target, running, dps
     while True:
         target = Gyro.get_abs_measure()%360
+        dps = Gyro.get_dps_measure()
         time.sleep(0.01)
 
 def maintain_angle(target):
@@ -57,25 +58,33 @@ if __name__ == "__main__":
 #     Gyro.reset_measure()
 #     maintain_angle(target)
     target = 0
+    dps = 0
     running = True
     t1 = Thread(target = continuous_measure_gyro)
     t1.start()
     
     #right turn overshoot
     start = target
+    print(start)
     motor_L.set_position_relative(343)
     motor_R.set_position_relative(-343)
+#     dps = 200 #set arbitrary val
+    while dps != 0:
+        turn = target - start
+    print(turn)
     time.sleep(0.1)
-    turn = target - start
-    error = 90-turn
-    if turn != 90:
-        motor_R.set_position_relative(-error)
-        motor_L.set_position_relative(error)
-#     This shit don't work yet
 
+#     error = 90-turn
+#     if turn != 90:
+#         motor_R.set_position_relative(-error)
+#         motor_L.set_position_relative(error)
+# #     This shit don't work yet
+# 
     while True:
-        print(target)
+#         print("target is " + str(target))
+        print("dps is " + str(dps))
         if TS1.is_pressed():
             break
+        time.sleep(0.1)
     t1.join()
     # print("reset")
