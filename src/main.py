3e7ba6dcfc9ee9ee99dbf8bd58684524fe89a49f
Hassan Shah <hassanashah10@gmail.com>
2026-04-05 14:38:27 -0400

    Investigation kind of complete but not really, need to test more to see behavior

--- src/main.py
+++ src/main.py
@@ -21,41 +21,43 @@ green_beds_found = 0
 bed_detection_threads_killed = False
 complete = False
 
+
 # CONFIRMED TO WORK
 def getMeds():
     # 1) Prepare claw to receive blocks
     open_gripper()
     time.sleep(0.5)
     # 2) Align claw to left block
-    rotate_with_gyro_correction(10, 300, LEFT) # in-built time.sleep()
+    rotate_with_gyro_correction(10, 300, LEFT)  # in-built time.sleep()
     # 3) Move towards left block
     move_dist_fwd(SQUARE_LENGTH * 0.4, 425)
     time.sleep(1.5)
     # 4) Left block in reach, store it
-    store_cube() # in-built time.sleep()
+    store_cube()  # in-built time.sleep()
     # 5) Move back
-    move_dist_fwd(-SQUARE_LENGTH * 3/8, 425)
+    move_dist_fwd(-SQUARE_LENGTH * 3 / 8, 425)
     time.sleep(1.5)
     # 6) Align claw to right block
-    rotate_with_gyro_correction(25, 300, RIGHT) # in-built time.sleep()
+    rotate_with_gyro_correction(25, 300, RIGHT)  # in-built time.sleep()
     # 7) Prepare for receival
     open_gripper()
     time.sleep(1)
     # 8) Move towards right block
-    move_dist_fwd(SQUARE_LENGTH * (3/8+0.05), 425)
+    move_dist_fwd(SQUARE_LENGTH * (3 / 8 + 0.05), 425)
     time.sleep(1)
     # 9) Grab instead of store, storage full
-    grab_cube() # in-built time.sleep()
-    move_dist_fwd(-SQUARE_LENGTH * 5.5/8, 425)
+    grab_cube()  # in-built time.sleep()
+    move_dist_fwd(-SQUARE_LENGTH * 5.5 / 8, 425)
     time.sleep(1)
     # 10) Re-align to initial alignment
-    rotate_with_gyro_correction(20, 300, LEFT) #in-built time.sleep()
+    rotate_with_gyro_correction(20, 300, LEFT)  #in-built time.sleep()
     # 11) Get robot in alignment with right line going outside of pharmacy
-    move_dist_fwd(SQUARE_LENGTH * -2/12, 425)
+    move_dist_fwd(SQUARE_LENGTH * -2 / 12, 425)
     time.sleep(3)
-    
-    rotate_with_gyro_correction(90,300, LEFT) # in-built time.sleep()
-    rotate_with_gyro_correction(4,300, RIGHT) # in-built time.sleep()
+
+    rotate_with_gyro_correction(90, 300, LEFT)  # in-built time.sleep()
+    rotate_with_gyro_correction(5, 300, RIGHT)  # in-built time.sleep()
+
 
 # CONFIRMED TO WORK
 def pharmacy_to_left_single():
@@ -63,9 +65,9 @@ def pharmacy_to_left_single():
     move_dist_fwd(SQUARE_LENGTH * 2, 425)
     time.sleep(3)
     rotate_with_gyro_correction(30, 300, LEFT)
-    move_dist_fwd(SQUARE_LENGTH * 0.2, 425)
+    move_dist_fwd(SQUARE_LENGTH * 0.15, 425)
     time.sleep(0.5)
-    rotate_with_gyro_correction(30,300,RIGHT)
+    rotate_with_gyro_correction(30, 300, RIGHT)
     # move_dist_fwd(SQUARE_LENGTH * -0.1, 425)
     time.sleep(1.5)
 
@@ -87,6 +89,7 @@ def right_single_to_double():
     rotate_with_gyro_correction(90, 300, RIGHT)
 
 
+# CONFIRMED TO WORK
 def inch_towards_door():
     global door_detected
     total_moved = 0
@@ -108,7 +111,8 @@ def inch_towards_door():
             # Move forward after having found the door
             move_dist_fwd(0.06, 425)
             break
-    return (total_moved+ 0.06)
+    return (total_moved + 0.06)
+
 
 def inch_away_from_door():
     global door_detected
@@ -134,7 +138,8 @@ def inch_away_from_door():
     return (total_moved + 0.06)
 
 
-def hassan_wiggle(amount : int):
+# DEPRECATED
+def hassan_wiggle(amount: int):
     total_to_rotate = amount
     remaining_to_rotate = amount
     global red_detected, green_detected
@@ -142,10 +147,10 @@ def hassan_wiggle(amount : int):
         if remaining_to_rotate == 0:
             print("wiggled full amount, no bed was found... ISSUE UH OH")
             break
-        arc_bot(total_to_rotate/10, 300, RIGHT)
+        arc_bot(total_to_rotate / 10, 300, RIGHT)
         time.sleep(1)
-        remaining_to_rotate -= total_to_rotate/10
-        print(f"Rotated {total_to_rotate/10} degrees for a partial wiggle")
+        remaining_to_rotate -= total_to_rotate / 10
+        print(f"Rotated {total_to_rotate / 10} degrees for a partial wiggle")
         print(f"Updated the remaining wiggle degree value to {remaining_to_rotate}")
         color_detected = classify_unknown_color()
         time.sleep(1)
@@ -161,6 +166,7 @@ def hassan_wiggle(amount : int):
     return (total_to_rotate, remaining_to_rotate)
 
 
+# DEPRECATED
 def process_room():
     global room_depth, green_beds_found, red_detected, green_detected
 
@@ -172,14 +178,15 @@ def process_room():
             print("Whoa max depth reached, gotta back outta there before you hurt yourself buddy")
             # Robot will be straight, can just go backwards
             move_dist_fwd(-SQUARE_LENGTH * room_depth, 425)
-            time.sleep(1.5*room_depth)
+            time.sleep(1.5 * room_depth)
             # Reset room depth in preparation for next process_room() call
             room_depth = 0
             print("moved back outta the room, we OUT now")
             break
         # Get the total desired amount to wiggle, and amount rotated until complete/bed found
         total_desired, total_remaining = hassan_wiggle(50)
-        print(f"The wiggle had a total desired amount of {total_desired}, completed {total_desired - total_remaining} and {total_remaining} remaining")
+        print(
+            f"The wiggle had a total desired amount of {total_desired}, completed {total_desired - total_remaining} and {total_remaining} remaining")
         # If a bed was found
         if red_detected or green_detected:
             print("BED WAS DETECTED LETS SEE WHICH KIND")
@@ -256,7 +263,7 @@ def return_from_double(section_number):
 
 def sonia_detect_bed_color():
     global red_detected, green_detected, bed_detection_threads_killed
-    while(not bed_detection_threads_killed):
+    while (not bed_detection_threads_killed):
         color_detected = classify_unknown_color()
         time.sleep(0.2)
         print("Scanned for a bed")
@@ -271,19 +278,19 @@ def sonia_detect_bed_color():
 
 def sonia_wiggle():
     global red_detected, green_detected, bed_detection_threads_killed, green_beds_found
-    max_wiggles = 7; # To explore a room in its entire depth, wiggle 7 times max (lowkey guessed this, untested still)
-    wiggle_counter = 0;
+    max_wiggles = 7  # To explore a room in its entire depth, wiggle 7 times max (lowkey guessed this, untested still)
+    wiggle_counter = 0
 
     while ((not red_detected) and (not green_detected) and (wiggle_counter < max_wiggles)):
         rotate_with_gyro_correction(50, 100, RIGHT)  # Wiggle right
         rotate_with_gyro_correction(50, 300, LEFT)  # Wiggle left
 
-        move_dist_fwd(0.03, 300)  # Move forward 7 cm before wiggling again
+        move_dist_fwd(0.07, 300)  # Move forward 7 cm before wiggling again
         time.sleep(0.5)
-        
-        wiggle_counter = wiggle_counter + 1;
 
-    if (green_detected): # If green is detected, drop block and move back
+        wiggle_counter += 1
+
+    if green_detected:  # If green is detected, drop block and move back
         release_cube()
         task_jingle()
         green_beds_found += 1
@@ -292,6 +299,7 @@ def sonia_wiggle():
     inch_away_from_door()
     bed_detection_threads_killed = True
     print("bed detection threads have been killed")
+
 
 def sonia_bed_detection():
     """This function essentially wiggles AND tries to identify a bed at the same time with threading"""
@@ -317,6 +325,7 @@ def sonia_bed_detection():
     green_detected = False
     print("We breaking outta the thread")
 
+
 if __name__ == "__main__":
     LEFT_WHEEL.set_limits(power=POWER_LIMIT, dps=425)
     RIGHT_WHEEL.set_limits(power=POWER_LIMIT, dps=425)
@@ -326,7 +335,7 @@ if __name__ == "__main__":
 
     # Step 2: pharmacy -> left single room [CONFIRMED]
     pharmacy_to_left_single()
-    
+
     # Step 3: Align to left single room door [CONFIRMED]
     total_door_adjustment = inch_towards_door()
     door_detected = False
@@ -334,31 +343,31 @@ if __name__ == "__main__":
     # Step 4: Find the bed in the left single room, deposit if green, and get out of room [CONFIRMED]
     move_dist_fwd(0.05, 300)
     time.sleep(1.5)
-    sonia_bed_detection() # Detects bed, deposits if green, gets out of room
+    sonia_bed_detection()  # Detects bed, deposits if green, gets out of room
     if green_beds_found == 1:
         retrieve_cube()
-    
+
     # Step 5: Move from left single to right single
     left_single_to_right_single()
-    
+
     # Step 6: Find bed in right single room, deposit if green, get out of room
     sonia_bed_detection()
-    
+
     # Check if both green beds found after checking both singles
     if green_beds_found == 2:
         return_from_right_single()
         victory_jingle()
-    
+
     # Step 7: Move to double room [FIX RETREAT]
     move_dist_fwd(-0.10, 300)
     time.sleep(1)
     right_single_to_double()
-    
+
     # Step 8: Scan double room section 1
     move_dist_fwd(0.05, 300)
     time.sleep(1.5)
-    sonia_bed_detection() # Detects bed, deposits if green, gets out of room
-    
+    sonia_bed_detection()  # Detects bed, deposits if green, gets out of room
+
     # Check if both green beds found after checking section 1 of double room
     if green_beds_found == 2:
         return_from_double(section_number=1)
@@ -377,7 +386,7 @@ if __name__ == "__main__":
     # Find bed in second section of the double room, deposit if green, and get out of room
     move_dist_fwd(0.05, 300)
     time.sleep(1.5)
-    sonia_bed_detection() # Detects bed, deposits if green, gets out of room
+    sonia_bed_detection()  # Detects bed, deposits if green, gets out of room
 
     # Return to position before having to find the room door
     move_dist_fwd(-total_door_adjustment, 425)
@@ -402,7 +411,7 @@ if __name__ == "__main__":
     # Find bed in second section of the double room, deposit if green, and get out of room
     move_dist_fwd(0.05, 300)
     time.sleep(1.5)
-    sonia_bed_detection() # Detects bed, deposits if green, gets out of room
+    sonia_bed_detection()  # Detects bed, deposits if green, gets out of room
 
     # Return to position before having to find the room door
     move_dist_fwd(-total_door_adjustment, 425)
@@ -411,5 +420,3 @@ if __name__ == "__main__":
     # At this point, it's guaranteed both beds are found
     return_from_double(section_number=3)
     victory_jingle()
-    
-
