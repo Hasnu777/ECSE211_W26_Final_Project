4bf446179fa01a427c5810a755d982b32beabc03
matt-yst <matthew.ys.tan@gmail.com>
2026-04-06 01:35:59 -0400

    Updated samsung washing machine jingle

--- src/robot_sound_system.py
+++ src/robot_sound_system.py
@@ -2,43 +2,16 @@ from utils.sound import Sound, Song
 from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sensors
 import threading
 import time
+import simpleaudio
 
 # CONSTANTS
-VOLUME = 90
-
-E6 = Sound(duration=0.3, pitch="E6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-E6_HALF = Sound(duration=0.3, pitch="E6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-E6_3HALVES = Sound(duration=0.45, pitch="E6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-E6_3X = Sound(duration=0.9, pitch="E6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-A7 = Sound(duration=0.3, pitch="A7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-A7_HALF = Sound(duration=0.15, pitch="A7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-A7_3X = Sound(duration=0.9, pitch="A7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-A7_2X = Sound(duration=0.6, pitch="A7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-C7 = Sound(duration=0.3, pitch="C#7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-B7 = Sound(duration=0.3, pitch="B7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-B7_HALF = Sound(duration=0.15, pitch="B7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-G6 = Sound(duration=0.3, pitch="G#6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-G6_HALF = Sound(duration=0.15, pitch="G#6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-F6_HALF = Sound(duration=0.15, pitch="F#6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-F6 = Sound(duration=0.3, pitch="F#6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-D6 = Sound(duration=0.3, pitch="D#6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-D7_HALF = Sound(duration=0.15, pitch="D#7", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-
-B5_COIN = Sound(duration=0.1, pitch="B5", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
-E6_COIN = Sound(duration=0.3, pitch="E6", volume=VOLUME, amp_f=5, amp_ka=0.1, amp_ac=0.9)
 
 
 # -------------------- FUNCTIONS --------------------
 def victory_jingle():
-    measure1 = [E6, A7, A7, C7, C7, A7_2X, E6, E6, E6_3HALVES, E6_HALF, B7_HALF, A7_HALF, G6_HALF, F6_HALF, E6_3X, E6,
-                A7, A7, C7, C7, A7_2X, E6, A7, G6, F6_HALF, G6_HALF, A7, D6, E6_3X, E6, G6, G6, A7_HALF, G6_HALF,
-                F6_HALF, G6_HALF, A7_2X, E6, A7, G6, G6, G6_HALF, D7_HALF, B7_HALF, G6_HALF, A7_3X, A7, F6, F6, F6, A7,
-                A7_2X, E6, E6, E6_3HALVES, E6_HALF, B7, G6, A7_3X, A7, G6_HALF, F6_HALF, F6, F6_HALF, A7_HALF, G6_HALF,
-                B7_HALF, A7_2X, E6, E6, E6_3HALVES, E6_HALF, B7, G6, A7_3X]
-    song1 = Song(measure1)
-    song1.compile()
-    song1.play()
-    song1.wait_done()
+    song = simpleaudio.WaveObject.from_wave_file("Samsung_washing_machine.wav")
+    song.play()
+    time.sleep(24)
 
 
 def task_jingle():
