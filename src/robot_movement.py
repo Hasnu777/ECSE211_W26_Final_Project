575d114bfb58fd49d141bcde389ef9f16c87ef30
DPM group 24 <dpm-24@example.com>
2026-04-08 04:52:32 +0000

    double room backing out not good - Matt

--- src/robot_movement.py
+++ src/robot_movement.py
@@ -123,6 +123,7 @@ def rotate_with_gyro_correction(turn_angle, speed, direction):
 
     # Make the robot turn
     angle_before_turn = GYRO.get_abs_measure()
+    angle_invalid = (angle_before_turn is None or not ((angle_before_turn < 180) and (angle_before_turn > -180)))
     while angle_before_turn is None:
         GYRO.reset_measure()
         angle_before_turn = GYRO.get_abs_measure()
